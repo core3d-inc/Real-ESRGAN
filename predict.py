199a2e43b93211a10da4698612dcdd0343c72b1e
Michael Giuliana <mike@rpnzl.com>
2024-03-08 20:47:20 -1000

    Expose tile, remove unused model and params

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path, File
+from cog import BasePredictor, Input, Path
 from basicsr.archs.rrdbnet_arch import RRDBNet
 import os, cv2
 import subprocess
@@ -9,8 +9,6 @@ import subprocess
 subprocess.call(['python', '/src/setup.py', 'develop'])
 
 from realesrgan import RealESRGANer
-from realesrgan.archs.srvgg_arch import SRVGGNetCompact
-from gfpgan import GFPGANer
 import tempfile
 
 model_name = 'RealESRGAN_x4plus'
@@ -18,16 +16,26 @@ model_path = os.path.join('/root/.cache/realesrgan', model_name + ".pth")
 
 class Predictor(BasePredictor):
     def setup(self):
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        self.upsampler = RealESRGANer(
-            scale=netscale,
-            model_path=model_path,
-            model=model,
-            tile=0,
-            tile_pad=10,
-            pre_pad=0,
-            half=True)
+        pass
+
+    def get_model(self, tile):
+        if not self.model:
+            self.model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+
+        if not self.models_by_tile:
+            self.models_by_tile = {}
+
+        if tile not in self.models_by_tile:
+            self.models_by_tile[tile] = RealESRGANer(
+                scale=4,
+                model_path=model_path,
+                model=self.model,
+                tile=tile,
+                tile_pad=10,
+                pre_pad=0,
+                half=True)
+
+        return self.models_by_tile[tile]
 
     def predict(
         self,
@@ -35,26 +43,15 @@ class Predictor(BasePredictor):
         scale: float = Input(
             description="Factor to scale image by", ge=0, le=10, default=4
         ),
-        face_enhance: bool = Input(description="Face enhance", default=True)
+        tile: int = Input(
+            description=
+            'Tile size. Default is 0, that is no tile. When encountering the out-of-GPU-memory issue, please specify, e.g. 256 or 512',
+            default=0)
     ) -> Path:
-
+        if tile <= 100 or tile is None:
+            tile = 0
         img = cv2.imread(str(image), cv2.IMREAD_UNCHANGED)
-        if len(img.shape) == 3 and img.shape[2] == 4:
-            img_mode = 'RGBA'
-        else:
-            img_mode = None
-
-        if face_enhance:
-            face_enhancer = GFPGANer(
-                model_path='/root/.cache/realesrgan/GFPGANv1.3.pth',
-                upscale=scale,
-                arch='clean',
-                channel_multiplier=2,
-                bg_upsampler=self.upsampler
-            )
-            _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
-        else:
-            output, _ = self.upsampler.enhance(img, outscale=scale)
+        output, _ = self.get_model(tile).enhance(img, outscale=scale)
         save_path=os.path.join(tempfile.mkdtemp(), "output.png")
         cv2.imwrite(save_path, output)
         return Path(save_path)
